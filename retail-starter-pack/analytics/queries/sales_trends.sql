8e5b16d954c2d4333e723af6b4994dd85d24c452
Biswadip Paul <bis.paul@gmail.com>
2024-04-23 23:28:37 +0530

    adding analytics for dashboards

---
+++ retail-starter-pack/analytics/queries/sales_trends.sql
@@ -0,0 +1,21 @@
+drop table if exists sales_trends_tmp;
+
+create table sales_trends_tmp as
+select * from sales_trends_tmp;
+
+select  * from sales_trends_tmp
+where date <> CAST(CAST(CURRENT_DATE as TIMESTAMP) as VARCHAR)
+union all
+SELECT
+CAST(CAST(CURRENT_DATE as TIMESTAMP) as VARCHAR) as date,
+APPROX_PERCENTILE(aov, .25) as aov_first_quartile,
+APPROX_PERCENTILE(aov, .50) as aov_median,
+APPROX_PERCENTILE(aov, .75) as aov_third_quartile,
+APPROX_PERCENTILE(ltv, .25) as ltv_first_quartile,
+APPROX_PERCENTILE(ltv, .50) as ltv_median,
+APPROX_PERCENTILE(ltv, .75) as ltv_third_quartile,
+APPROX_PERCENTILE(avg_days_between_transactions, .25) as avg_days_between_transactions_first_quartile,
+APPROX_PERCENTILE(avg_days_between_transactions, .50) as avg_days_between_transactions_median,
+APPROX_PERCENTILE(avg_days_between_transactions, .75) as avg_days_between_transactions_third_quartile
+from
+${src_database}.derived_attributes;
