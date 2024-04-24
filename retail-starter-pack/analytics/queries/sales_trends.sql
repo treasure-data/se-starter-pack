7061cbf5ee0bb7489fa9fae46aad682f3667c274
Biswadip Paul <bis.paul@gmail.com>
2024-04-24 12:21:09 +0530

    adding analytics

--- retail-starter-pack/analytics/queries/sales_trends.sql
+++ retail-starter-pack/analytics/queries/sales_trends.sql
@@ -1,12 +1,22 @@
 drop table if exists sales_trends_tmp;
 
 create table sales_trends_tmp as
-select * from sales_trends_tmp;
+select * from sales_trends;
 
-select  * from sales_trends_tmp
+select  date,
+aov_first_quartile,
+aov_median,
+aov_third_quartile,
+ltv_first_quartile,
+ltv_median,
+ltv_third_quartile,
+avg_days_between_transactions_first_quartile,
+avg_days_between_transactions_median,
+avg_days_between_transactions_third_quartile
+from sales_trends_tmp
 where date <> CAST(CAST(CURRENT_DATE as TIMESTAMP) as VARCHAR)
 union all
-SELECT
+select
 CAST(CAST(CURRENT_DATE as TIMESTAMP) as VARCHAR) as date,
 APPROX_PERCENTILE(aov, .25) as aov_first_quartile,
 APPROX_PERCENTILE(aov, .50) as aov_median,
