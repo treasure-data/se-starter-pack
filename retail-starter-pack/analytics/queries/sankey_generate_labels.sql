de4d5946944ff721587813dee76c63e0881f6182
Biswadip Paul <bis.paul@gmail.com>
2024-05-24 15:37:35 +0530

    CI/CD debug

--- retail-starter-pack/analytics/queries/sankey_generate_labels.sql
+++ retail-starter-pack/analytics/queries/sankey_generate_labels.sql
@@ -1,5 +1,6 @@
 
-  select "from" as label, ROW_NUMBER() OVER() - 1 as row_num from
+select "from" as label, ROW_NUMBER() OVER() - 1 as row_num
+from
   (
     select "from" from web_conversion_step_statistics
     union
