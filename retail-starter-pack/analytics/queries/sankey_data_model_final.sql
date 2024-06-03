8d4bfc2962a09c285a82a671d9eeb0aed089e99c
Biswadip Paul <bis.paul@gmail.com>
2024-06-03 21:41:25 +0530

    Web Journey Sankey Dashboard

--- retail-starter-pack/analytics/queries/sankey_data_model_final.sql
+++ retail-starter-pack/analytics/queries/sankey_data_model_final.sql
@@ -1,14 +1,7 @@
 select * from
 (
-  select null as label, t1.value, t2.row_num as from_num, t3.row_num as to_num, stage
-  from  web_conversion_step_statistics t1
-  left join
-  web_conversion_labels t2
-  on t1."from" = t2.label
-  left join
-  web_conversion_labels t3
-  on t1.to = t3.label
-)
-union all
-select CONCAT(LPAD(CAST(row_num as VARCHAR), 3, '0'), ':', label), NULL as value, NULL as from_num, NULL as to_num, NULL AS stage
-from web_conversion_labels
+  select null as label, t1.value, t1.from_num as from_num, t1.to_num as to_num, cast(NULL as varchar) as stage from web_conversion_step_statistics t1
+  union all
+  select CONCAT(LPAD(CAST(row_num as VARCHAR), 3, '0'), ':', label) as label, NULL as value, NULL as from_num, NULL as to_num, cast(NULL as varchar) AS stage
+  from web_conversion_labels
+);
--- retail-starter-pack/analytics/queries/sankey_generate_labels.sql
+++ retail-starter-pack/analytics/queries/sankey_generate_labels.sql
@@ -1,8 +1,24 @@
-
-select "from" as label, ROW_NUMBER() OVER() - 1 as row_num
-from
-  (
-    select "from" from web_conversion_step_statistics
-    union
-    select "to" from web_conversion_step_statistics
-  )
+with lbl as (
+  select '${analytics.dashboards.sankey.register.stage}' as label, cast('${analytics.dashboards.sankey.register.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.login.stage}' as label, cast('${analytics.dashboards.sankey.login.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.products.stage}' as label, cast('${analytics.dashboards.sankey.products.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.wishlist.stage}' as label, cast('${analytics.dashboards.sankey.wishlist.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.cart.stage}' as label, cast('${analytics.dashboards.sankey.cart.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.checkout.stage}' as label, cast('${analytics.dashboards.sankey.checkout.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.loyalty.stage}' as label, cast('${analytics.dashboards.sankey.loyalty.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.return.stage}' as label, cast('${analytics.dashboards.sankey.return.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.review.stage}' as label, cast('${analytics.dashboards.sankey.review.indexno}' as int) as row_num
+  union
+  select '${analytics.dashboards.sankey.support.stage}' as label, cast('${analytics.dashboards.sankey.support.indexno}' as int) as row_num
+)
+select label, row_num
+from lbl
+order by row_num
