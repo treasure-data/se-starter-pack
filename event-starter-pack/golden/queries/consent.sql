62767d86d089868ddb4993b9bc32a2c40afa8953
Biswadip Paul <bis.paul@gmail.com>
2024-06-24 17:32:23 +0530

    consent sql fix

--- event-starter-pack/golden/queries/consent.sql
+++ event-starter-pack/golden/queries/consent.sql
@@ -1,17 +1,17 @@
 select
-trfmd_email as id,
-trfmd_id_type,
-trfmd_consent_type,
-event_unification_id,
-trfmd_consent_flag
+  trfmd_email as id
+, trfmd_id_type
+, trfmd_consent_type
+, event_unification_id
+, trfmd_consent_flag
 from enriched_consents_email
 
 UNION ALL
 
 select
-trfmd_phone_number as id,
-trfmd_id_type,
-trfmd_consent_type,
-event_unification_id,
-event_consent_flag
+  trfmd_phone_number as id
+, trfmd_id_type
+, trfmd_consent_type
+, event_unification_id
+, trfmd_consent_flag
 from enriched_consents_phone
