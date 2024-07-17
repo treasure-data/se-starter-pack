942029f5ea3c9a8dc6b4f6082996bf246e45d8b0
Biswadip Paul <bis.paul@gmail.com>
2024-07-17 13:40:07 +0530

    Using TD INTERVAL funcition

--- retail-starter-pack/analytics/queries/web_analytics_agg.sql
+++ retail-starter-pack/analytics/queries/web_analytics_agg.sql
@@ -1,27 +1,22 @@
-with cal as (
-  select DATE_FORMAT(date_add('day', -1, current_date), '%Y-%m-%d') st_dt,
-            DATE_FORMAT(current_date, '%Y-%m-%d') end_dt,
-          DATE_FORMAT(current_date,  '%Y-%m-%d 00:00:00.0') today_datetime
-)
-,known_cust as (
+with known_cust as (
   select distinct retail_unification_id as  kc
   from enriched_customers
 ),
 all_web_cust as (
   select distinct retail_unification_id as wc
-  from enriched_pageviews, cal
-  where TD_TIME_RANGE(time, st_dt , end_dt)
+  from enriched_pageviews
+  where TD_INTERVAL(time, '-1d')
 ),
 total_sales as (
 (
   select sum(amount) as amount from (
       select sum(amount) as amount
-        from enriched_order_offline_transactions, cal
-        where TD_TIME_RANGE(trfmd_order_datetime_unix, st_dt , end_dt)
+        from enriched_order_offline_transactions
+        where TD_INTERVAL(trfmd_order_datetime_unix, '-1d')
         union all
         select sum(amount) as amount
-        from enriched_order_online_transactions, cal
-        where TD_TIME_RANGE(trfmd_order_datetime_unix, st_dt , end_dt)
+        from enriched_order_online_transactions
+        where TD_INTERVAL(trfmd_order_datetime_unix, '-1d')
   )
 )
 ),
@@ -31,5 +26,9 @@ new_cust_cnt as (
   on (a.kc = b.wc)
   where a.kc is null
 )
-select today_datetime as run_date, known_cust_cnt,  new_customer_cnt, round(amount, 2) total_sales
-from (select count(kc) as known_cust_cnt from known_cust) , new_cust_cnt, total_sales, cal
+select
+  DATE_FORMAT(current_date,  '%Y-%m-%d 00:00:00.0') as run_date
+, known_cust_cnt
+, new_customer_cnt
+, round(amount, 2) total_sales
+from (select count(kc) as known_cust_cnt from known_cust) , new_cust_cnt, total_sales
