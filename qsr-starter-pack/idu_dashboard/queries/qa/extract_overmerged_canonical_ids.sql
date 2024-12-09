5186604ca5ef2c91fa52ec9dc99f6840396f7e87
Vishal Patel <vishalpatel2890@gmail.com>
2024-12-09 07:05:09 -0500

    Update extract_overmerged_canonical_ids.sql

--- qsr-starter-pack/idu_dashboard/queries/qa/extract_overmerged_canonical_ids.sql
+++ qsr-starter-pack/idu_dashboard/queries/qa/extract_overmerged_canonical_ids.sql
@@ -2,3 +2,5 @@ SELECT canonical_id AS over_merged_id, count(*) as count_raw_ids from ${id_looku
 ${td.last_results.filter_syntax}
 GROUP BY 1 
 HAVING count(*) >= ${td.last_results.merged_ids_limit}
+ORDER BY count(*) desc
+limit 100000
