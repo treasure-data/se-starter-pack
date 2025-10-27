39cc8b397bc1fea0ec28358f443e23241b497d60
Vishal Patel <vishalpatel2890@gmail.com>
2025-10-27 08:54:29 -0400

    update for soft search on id in ps tempalte

--- qsr-starter-pack/segment/python/api/ps.py
+++ qsr-starter-pack/segment/python/api/ps.py
@@ -54,26 +54,29 @@ def getParentSegment(body):
         print(_body)
         response.raise_for_status()
         if response.ok:
+            _body_id = _body.get("id")
+            _body_name = _body.get("name")
+
             for row in data:
                 print(row["id"])
-                print(_body["id"])
-                if _body["id"] and str(row["id"]) == str(_body["id"]):
+                print(_body_id)
+                if _body_id and str(row["id"]) == str(_body_id):
                     print(f"Found Parent Segment by id {row.get('id')}")
                     return (row.get("id"), row.get("name"), "selected")
                 elif (
-                    not _body["id"]
-                    and _body["name"]
-                    and str(row["name"]) == str(_body["name"])
+                    not _body_id
+                    and _body_name
+                    and str(row["name"]) == str(_body_name)
                 ):
                     print(f"Found Parent Segment by name {row.get('id')}")
                     return (row.get("id"), row.get("name"), "selected")
-                elif not _body["id"] and not _body["name"]:
+                elif not _body_id and not _body_name:
                     print(
                         "Atleast name or id should be provide in the parent segment template yml file."
                     )
                     break
             # No Matches
-            print(f"Not Found Parent Segment {_body['id']}")
+            print(f"Not Found Parent Segment {_body_id}")
             return None, None, "selected"
     except Exception as ex:
         # print(f"Post API create Parent Segment failed: {response.text}")
